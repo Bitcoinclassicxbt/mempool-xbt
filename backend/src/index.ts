99e7e51cccde2f16d36c2253625ca15794909bd1
Jorge Lara <jelch2002@gmail.com>
2025-04-04 12:51:02 -0600

    debug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -73,7 +73,11 @@ class Server {
     }
 
     if (cluster.isPrimary) {
-      logger.notice(`Mempool Server (Master) is running on port ${config.MEMPOOL.HTTP_PORT} (${backendInfo.getShortCommitHash()})`);
+      logger.notice(
+        `Mempool Server (Master) is running on port ${
+          config.MEMPOOL.HTTP_PORT
+        } (${backendInfo.getShortCommitHash()})`
+      );
 
       const numCPUs = config.MEMPOOL.SPAWN_CLUSTER_PROCS;
       for (let i = 0; i < numCPUs; i++) {
@@ -84,7 +88,13 @@ class Server {
 
       cluster.on('exit', (worker, code, signal) => {
         const workerId = worker.process['env'].workerId;
-        logger.warn(`Mempool Worker PID #${worker.process.pid} workerId: ${workerId} died. Restarting in 10 seconds... ${signal || code}`);
+        logger.warn(
+          `Mempool Worker PID #${
+            worker.process.pid
+          } workerId: ${workerId} died. Restarting in 10 seconds... ${
+            signal || code
+          }`
+        );
         setTimeout(() => {
           const env = { workerId: workerId };
           const newWorker = cluster.fork(env);
@@ -100,12 +110,20 @@ class Server {
     const tags = await getTags();
     console.log(tags);
 
-    logger.notice(`Starting Mempool Server${worker ? ' (worker)' : ''}... (${backendInfo.getShortCommitHash()})`);
+    logger.notice(
+      `Starting Mempool Server${
+        worker ? ' (worker)' : ''
+      }... (${backendInfo.getShortCommitHash()})`
+    );
 
     // Register cleanup listeners for exit events
-    ['exit', 'SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2'].forEach(event => {
-      process.on(event, () => { this.onExit(event); });
-    });
+    ['exit', 'SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2'].forEach(
+      (event) => {
+        process.on(event, () => {
+          this.onExit(event);
+        });
+      }
+    );
     process.on('uncaughtException', (error) => {
       this.onUnhandledException('uncaughtException', error);
     });
@@ -122,7 +140,8 @@ class Server {
 
       await DB.checkDbConnection();
       try {
-        if (process.env.npm_config_reindex_blocks === 'true') { // Re-index requests
+        if (process.env.npm_config_reindex_blocks === 'true') {
+          // Re-index requests
           await databaseMigration.$blocksReindexingTruncate();
         }
         await databaseMigration.$initializeOrMigrateDatabase();
@@ -138,8 +157,7 @@ class Server {
       })
       .use(express.urlencoded({ extended: true }))
       .use(express.text({ type: ['text/plain', 'application/base64'] }))
-      .use(express.json())
-      ;
+      .use(express.json());
 
     if (config.DATABASE.ENABLED && config.FIAT_PRICE.ENABLED) {
       await priceUpdater.$initializeLatestPriceWithDb();
@@ -149,7 +167,9 @@ class Server {
     this.wss = new WebSocket.Server({ server: this.server });
     if (config.MEMPOOL.UNIX_SOCKET_PATH) {
       this.serverUnixSocket = http.createServer(this.app);
-      this.wssUnixSocket = new WebSocket.Server({ server: this.serverUnixSocket });
+      this.wssUnixSocket = new WebSocket.Server({
+        server: this.serverUnixSocket,
+      });
     }
 
     this.setUpWebsocketHandling();
@@ -165,7 +185,11 @@ class Server {
       }
     }
 
-    if (config.STATISTICS.ENABLED && config.DATABASE.ENABLED && cluster.isPrimary) {
+    if (
+      config.STATISTICS.ENABLED &&
+      config.DATABASE.ENABLED &&
+      cluster.isPrimary
+    ) {
       statistics.startStatistics();
     }
 
@@ -174,7 +198,10 @@ class Server {
         try {
           icons.loadIcons();
         } catch (e) {
-          logger.err('Cannot load liquid icons. Ignoring. Reason: ' + (e instanceof Error ? e.message : e));
+          logger.err(
+            'Cannot load liquid icons. Ignoring. Reason: ' +
+              (e instanceof Error ? e.message : e)
+          );
         }
       };
       // Run once on startup.
@@ -194,7 +221,9 @@ class Server {
       this.runMainUpdateLoop();
     }
 
-    setInterval(() => { this.healthCheck(); }, 2500);
+    setInterval(() => {
+      this.healthCheck();
+    }, 2500);
 
     if (config.LIGHTNING.ENABLED) {
       this.$runLightningBackend();
@@ -204,7 +233,9 @@ class Server {
       if (worker) {
         logger.info(`Mempool Server worker #${process.pid} started`);
       } else {
-        logger.notice(`Mempool Server is running on port ${config.MEMPOOL.HTTP_PORT}`);
+        logger.notice(
+          `Mempool Server is running on port ${config.MEMPOOL.HTTP_PORT}`
+        );
       }
     });
 
@@ -213,7 +244,9 @@ class Server {
         if (worker) {
           logger.info(`Mempool Server worker #${process.pid} started`);
         } else {
-          logger.notice(`Mempool Server is listening on ${config.MEMPOOL.UNIX_SOCKET_PATH}`);
+          logger.notice(
+            `Mempool Server is listening on ${config.MEMPOOL.UNIX_SOCKET_PATH}`
+          );
         }
       });
     }
@@ -227,21 +260,44 @@ class Server {
       try {
         await memPool.$updateMemPoolInfo();
       } catch (e) {
-        const msg = `updateMempoolInfo: ${(e instanceof Error ? e.message : e)}`;
+        const msg = `updateMempoolInfo: ${e instanceof Error ? e.message : e}`;
         if (config.MEMPOOL.USE_SECOND_NODE_FOR_MINFEE) {
           logger.warn(msg);
         } else {
           logger.debug(msg);
         }
       }
+
+      console.log('a');
       const newMempool = await bitcoinApi.$getRawMempool();
-      const minFeeMempool = memPool.limitGBT ? await bitcoinSecondClient.getRawMemPool() : null;
-      const minFeeTip = memPool.limitGBT ? await bitcoinSecondClient.getBlockCount() : -1;
+      console.log('b');
+
+      const minFeeMempool = memPool.limitGBT
+        ? await bitcoinSecondClient.getRawMemPool()
+        : null;
+      console.log('c');
+
+      const minFeeTip = memPool.limitGBT
+        ? await bitcoinSecondClient.getBlockCount()
+        : -1;
+      console.log('d');
+
       const latestAccelerations = await accelerationApi.$updateAccelerations();
+      console.log('e');
+
       const numHandledBlocks = await blocks.$updateBlocks();
-      const pollRate = config.MEMPOOL.POLL_RATE_MS * (indexer.indexerIsRunning() ? 10 : 1);
+      console.log('f');
+
+      const pollRate =
+        config.MEMPOOL.POLL_RATE_MS * (indexer.indexerIsRunning() ? 10 : 1);
       if (numHandledBlocks === 0) {
-        await memPool.$updateMempool(newMempool, latestAccelerations, minFeeMempool, minFeeTip, pollRate);
+        await memPool.$updateMempool(
+          newMempool,
+          latestAccelerations,
+          minFeeMempool,
+          minFeeTip,
+          pollRate
+        );
       }
       indexer.$run();
       if (config.WALLETS.ENABLED) {
@@ -255,12 +311,15 @@ class Server {
       // rerun immediately if we skipped the mempool update, otherwise wait POLL_RATE_MS
       const elapsed = Date.now() - start;
       const remainingTime = Math.max(0, pollRate - elapsed);
-      setTimeout(this.runMainUpdateLoop.bind(this), numHandledBlocks > 0 ? 0 : remainingTime);
+      setTimeout(
+        this.runMainUpdateLoop.bind(this),
+        numHandledBlocks > 0 ? 0 : remainingTime
+      );
       this.backendRetryCount = 0;
     } catch (e: any) {
       this.backendRetryCount++;
       let loggerMsg = `Exception in runMainUpdateLoop() (count: ${this.backendRetryCount}). Retrying in ${this.currentBackendRetryInterval} sec.`;
-      loggerMsg += ` Reason: ${(e instanceof Error ? e.message : e)}.`;
+      loggerMsg += ` Reason: ${e instanceof Error ? e.message : e}.`;
       if (e?.stack) {
         loggerMsg += ` Stack trace: ${e.stack}`;
       }
@@ -275,7 +334,10 @@ class Server {
       if (e instanceof AxiosError) {
         logger.debug(`AxiosError: ${e?.message}`);
       }
-      setTimeout(this.runMainUpdateLoop.bind(this), 1000 * this.currentBackendRetryInterval);
+      setTimeout(
+        this.runMainUpdateLoop.bind(this),
+        1000 * this.currentBackendRetryInterval
+      );
     } finally {
       diskCache.unlock();
     }
@@ -287,11 +349,15 @@ class Server {
       await networkSyncService.$startService();
       await lightningStatsUpdater.$startService();
       await forensicsService.$startService();
-    } catch(e) {
-      logger.err(`Exception in $runLightningBackend. Restarting in 1 minute. Reason: ${(e instanceof Error ? e.message : e)}`);
+    } catch (e) {
+      logger.err(
+        `Exception in $runLightningBackend. Restarting in 1 minute. Reason: ${
+          e instanceof Error ? e.message : e
+        }`
+      );
       await Common.sleep$(1000 * 60);
       this.$runLightningBackend();
-    };
+    }
   }
 
   setUpWebsocketHandling(): void {
@@ -308,20 +374,32 @@ class Server {
           await elementsParser.$parse();
           await elementsParser.$updateFederationUtxos();
         } catch (e) {
-          logger.warn('Elements parsing error: ' + (e instanceof Error ? e.message : e));
+          logger.warn(
+            'Elements parsing error: ' + (e instanceof Error ? e.message : e)
+          );
         }
       });
     }
     websocketHandler.setupConnectionHandling();
     if (config.MEMPOOL.ENABLED) {
-      statistics.setNewStatisticsEntryCallback(websocketHandler.handleNewStatistic.bind(websocketHandler));
-      memPool.setAsyncMempoolChangedCallback(websocketHandler.$handleMempoolChange.bind(websocketHandler));
-      blocks.setNewAsyncBlockCallback(websocketHandler.handleNewBlock.bind(websocketHandler));
+      statistics.setNewStatisticsEntryCallback(
+        websocketHandler.handleNewStatistic.bind(websocketHandler)
+      );
+      memPool.setAsyncMempoolChangedCallback(
+        websocketHandler.$handleMempoolChange.bind(websocketHandler)
+      );
+      blocks.setNewAsyncBlockCallback(
+        websocketHandler.handleNewBlock.bind(websocketHandler)
+      );
     }
     if (config.FIAT_PRICE.ENABLED) {
-      priceUpdater.setRatesChangedCallback(websocketHandler.handleNewConversionRates.bind(websocketHandler));
+      priceUpdater.setRatesChangedCallback(
+        websocketHandler.handleNewConversionRates.bind(websocketHandler)
+      );
     }
-    loadingIndicators.setProgressChangedCallback(websocketHandler.handleLoadingChanged.bind(websocketHandler));
+    loadingIndicators.setProgressChangedCallback(
+      websocketHandler.handleLoadingChanged.bind(websocketHandler)
+    );
 
     accelerationApi.connectWebsocket();
   }
@@ -330,7 +408,11 @@ class Server {
     bitcoinRoutes.initRoutes(this.app);
     bitcoinCoreRoutes.initRoutes(this.app);
     pricesRoutes.initRoutes(this.app);
-    if (config.STATISTICS.ENABLED && config.DATABASE.ENABLED && config.MEMPOOL.ENABLED) {
+    if (
+      config.STATISTICS.ENABLED &&
+      config.DATABASE.ENABLED &&
+      config.MEMPOOL.ENABLED
+    ) {
       statisticsRoutes.initRoutes(this.app);
     }
     if (Common.indexingEnabled() && config.MEMPOOL.ENABLED) {
@@ -361,14 +443,32 @@ class Server {
     this.maxHeapSize = Math.max(stats.used_heap_size, this.maxHeapSize);
     const warnThreshold = 0.8 * stats.heap_size_limit;
 
-    const byteUnits = getBytesUnit(Math.max(this.maxHeapSize, stats.heap_size_limit));
+    const byteUnits = getBytesUnit(
+      Math.max(this.maxHeapSize, stats.heap_size_limit)
+    );
 
     if (!this.warnedHeapCritical && this.maxHeapSize > warnThreshold) {
       this.warnedHeapCritical = true;
-      logger.warn(`Used ${(this.maxHeapSize / stats.heap_size_limit * 100).toFixed(2)}% of heap limit (${formatBytes(this.maxHeapSize, byteUnits, true)} / ${formatBytes(stats.heap_size_limit, byteUnits)})!`);
+      logger.warn(
+        `Used ${((this.maxHeapSize / stats.heap_size_limit) * 100).toFixed(
+          2
+        )}% of heap limit (${formatBytes(
+          this.maxHeapSize,
+          byteUnits,
+          true
+        )} / ${formatBytes(stats.heap_size_limit, byteUnits)})!`
+      );
     }
-    if (this.lastHeapLogTime === null || (now - this.lastHeapLogTime) > (this.heapLogInterval * 1000)) {
-      logger.debug(`Memory usage: ${formatBytes(this.maxHeapSize, byteUnits)} / ${formatBytes(stats.heap_size_limit, byteUnits)}`);
+    if (
+      this.lastHeapLogTime === null ||
+      now - this.lastHeapLogTime > this.heapLogInterval * 1000
+    ) {
+      logger.debug(
+        `Memory usage: ${formatBytes(
+          this.maxHeapSize,
+          byteUnits
+        )} / ${formatBytes(stats.heap_size_limit, byteUnits)}`
+      );
       this.warnedHeapCritical = false;
       this.maxHeapSize = 0;
       this.lastHeapLogTime = now;
